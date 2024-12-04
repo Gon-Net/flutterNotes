6d2251f3ddc0b9019d7f52f6d2c14c2caf381339
jcori <gonzalocori@gmail.com>
2024-12-04 14:57:22 -0400

    Creation apk notes

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:app_notes/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Demo',
+      home: const HomePage(),
+    );
+  }
+}
